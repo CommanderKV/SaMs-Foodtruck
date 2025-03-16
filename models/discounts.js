bee8848bc764100a637dc5b8d0849b59e9681641
CommanderKV <kylervisser@gmail.com>
2025-03-15 20:56:46 -0400

    Update priceAdjustment validation to require a minimum value of 0.01

--- models/discounts.js
+++ models/discounts.js
@@ -19,9 +19,9 @@ export default (sequelize, DataTypes) => {
 			priceAdjustment: {
 				type: DataTypes.DECIMAL(10, 2),
 				allowNull: false,
-                validate: {
-                    min: 0,
-                },
+				validate: {
+					min: 0.01,
+				},
 			}
 		}
 	);
